99abcf2ba912c2a37d91fa2b7162589ce608ab3c
rloraine <rloraine@student.21-school.ru>
2021-02-18 16:49:08 +0300

    del trash in main

--- main.py
+++ main.py
@@ -5,9 +5,3 @@ if __name__ == '__main__':
     game = Game()
     game.load_game()
     game.run()
-
-
-# if __name__ == '__main__':
-#     from  src.display.display_rework import Display
-#
-#     Display()
